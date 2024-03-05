2e9b761357569485e08096327cd5d068ca23ae73
Yalou09 <yaellejehu@gmail.com>
2024-03-05 12:26:41 +0100

    image riz

--- src/templates/post_savor.php
+++ src/templates/post_savor.php
@@ -1,11 +1,7 @@
 <?php
 include('./src/controller/SavorController.php');
 $savor = new SavorController;
-
-
-$datas = json_decode(file_get_contents('php://input'), true);
-
-
+$datas = json_decode(file_get_contents('php://input'), false);
 foreach ($datas as $data) {
    echo $savor->postSavor($data);
 }
